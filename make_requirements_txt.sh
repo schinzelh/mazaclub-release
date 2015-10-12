01bbed5c38531338fe5329c3d3b6b42998d5195b
Rob Nelson <guruvan@maza.club>
2015-10-12 15:21:19 -0700

    improve make_requirements_txt - add list of std mazaclub dep versions

--- make_requirements_txt.sh
+++ make_requirements_txt.sh
@@ -4,32 +4,49 @@ rm reqs.txt deplinx.txt
  while read line ; do 
     x=$(echo ${line}| awk -F\( '{print $1}') ; 
     if [ "${x}" = "setup" ] ; then 
-       echo "in setup" ; 
        y=1; 
     fi; 
     if [ "${y}" = "1" ] ; then 
        echo ${line} >> reqs.txt ; 
     fi; 
     if [ "${line}" = "]," ] ; then 
-        echo "out of setup" ;
 	y=0 ; 
     fi ;
  done < ../../setup.py 
  while read line ; do 
     x=$(echo ${line}| sed 's/[ 	][ 	]*//g'|awk -F\= '{print $1}') ; 
     if [ "${x}" = "dependency_links" ] ; then 
-       echo "in setup" ; 
        y=1; 
     fi; 
     if [ ${y} = 1 ] ; then 
        echo ${line} >> deplinx.txt 
     fi; 
     if [ "${line}" = "]," ] ; then 
-       echo "out of setup" ;
        y=0 ; 
     fi ;
  done < ../../setup.py 
  cat reqs.txt |egrep -v 'trezor|version|setup|name|install_|dependency_' |egrep -v '\[|\]'|sed -e 's/\,//g' -e 's/'\''//g' > helpers/requirements.txt
  cat deplinx.txt |egrep -v '\[|\]'|sed -e 's/'\"'//g'  -e  's/'\''//g' -e 's/\,//g'  >> helpers/requirements.txt
 rm reqs.txt deplinx.txt
+cp helpers/requirements.txt helpers/requirements.tmp
+while read dep ; do
+   ver=$(echo ${dep} | sed 's/[\<\>]*//g' | awk -F\= '{print $2}')
+   req_dep=$(echo ${dep} | sed 's/[\<\>]*//g' | awk -F\= '{print $1}' |sed 's/\=//g')
+   echo $ver
+   echo ${req_dep}
+   if [ "${ver}X" = "X" ] ; then
+      echo "null version"
+      std_ver=$(grep ${req_dep} helpers/pip_requirements.ver | awk '{print $2}')
+      echo $std_ver
+      sed -i -e 's/'${dep}'/'${req_dep}'\=\='${std_ver}'/g' helpers/requirements.txt
+   fi
+   mv helpers/requirements.txt helpers/req.tmp
+   sort -u helpers/req.tmp > helpers/requirements.txt
+done < helpers/requirements.tmp
+rm helpers/requirements.tmp
+rm helpers/req.tmp
+
+   
+     
+
 
