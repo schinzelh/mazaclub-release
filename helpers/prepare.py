c65575e8dec0f94bb5607e5c71ccfbb1ff2b5f2f
Holger Schinzel <holger.schinzel@gmx.de>
2016-02-29 14:52:35 +0100

    Update prepare.py

--- helpers/prepare.py
+++ helpers/prepare.py
@@ -19,10 +19,9 @@ if _platform == "linux" or _platform == "linux2":
 elif _platform == "darwin":
     # MAC OS X
     call(["/usr/local/bin/pyrcc4", "icons.qrc", "-o" ,  "gui/qt/icons_rc.py"])
-
-    with tarfile.TarFile('artifacts/dist/trezor-0.6.7.macosx-10.9-x86_64.tar.gz', "r:gz") as z:
-        z.extractall("artifacts")
-
+    
+    tar = tarfile.open("artifacts/dist/trezor-0.6.7.macosx-10.9-x86_64.tar.gz", "r:gz")
+    tar.extractall("artifacts")
 
 
 elif _platform == "win32":
