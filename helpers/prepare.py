3a594b4d5adec5b9cd08183c1aa8acbe01ba2e61
Holger Schinzel <holger.schinzel@gmx.de>
2016-02-29 13:45:22 +0100

    Update prepare.py

--- helpers/prepare.py
+++ helpers/prepare.py
@@ -2,6 +2,7 @@ import shutil
 import os
 import zipfile
 
+from sys import platform as _platform
 from subprocess import call
 
 def touch(fname, times=None):
@@ -10,7 +11,17 @@ def touch(fname, times=None):
 
 touch('packages/google/__init__.py')
 
-call(["C:\Python27\Lib\site-packages\PyQt4\pyrcc4.exe", "icons.qrc", "-o" ,  "gui/qt/icons_rc.py"])
+
+if _platform == "linux" or _platform == "linux2":
+   # linux
+elif _platform == "darwin":
+   # MAC OS X
+   call(["/usr/local/bin/pyrcc4", "icons.qrc", "-o" ,  "gui/qt/icons_rc.py"])
+elif _platform == "win32":
+   # Windows
+   call(["C:\Python27\Lib\site-packages\PyQt4\pyrcc4.exe", "icons.qrc", "-o" ,  "gui/qt/icons_rc.py"])
+
+
 
 with zipfile.ZipFile('artifacts/dist/trezor-0.6.7.win32.zip', "r") as z:
     z.extractall("artifacts")
