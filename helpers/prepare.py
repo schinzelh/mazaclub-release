b2cf95b82398b22acacfcdc73af95eb47d43a9a7
Holger Schinzel <holger.schinzel@gmx.de>
2016-02-29 14:48:47 +0100

    Update prepare.py

--- helpers/prepare.py
+++ helpers/prepare.py
@@ -1,6 +1,7 @@
 import shutil
 import os
 import zipfile
+import tarfile
 
 from sys import platform as _platform
 from subprocess import call
@@ -18,15 +19,18 @@ if _platform == "linux" or _platform == "linux2":
 elif _platform == "darwin":
     # MAC OS X
     call(["/usr/local/bin/pyrcc4", "icons.qrc", "-o" ,  "gui/qt/icons_rc.py"])
+
+    with tarfile.TarFile('artifacts/dist/trezor-0.6.7.macosx-10.9-x86_64.tar.gz', "r:gz") as z:
+        z.extractall("artifacts")
+
+
+
 elif _platform == "win32":
     # Windows
     call(["C:\Python27\Lib\site-packages\PyQt4\pyrcc4.exe", "icons.qrc", "-o" ,  "gui/qt/icons_rc.py"])
-
-
-
-with zipfile.ZipFile('artifacts/dist/trezor-0.6.7.win32.zip', "r") as z:
-    z.extractall("artifacts")
-
-shutil.copy2('artifacts/Python27/Scripts/trezorctl', 'packages/trezorctl.py')
+    
+    with zipfile.ZipFile('artifacts/dist/trezor-0.6.7.win32.zip', "r") as z:
+        z.extractall("artifacts")
+    shutil.copy2('artifacts/Python27/Scripts/trezorctl', 'packages/trezorctl.py')
 
 shutil.copytree('packages/requests', 'requests')
