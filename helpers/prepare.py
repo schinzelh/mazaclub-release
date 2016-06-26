3c3024332eff57c763469658ea6fbd92087ccb50
Holger Schinzel <holger.schinzel@gmx.de>
2016-06-26 13:19:43 +0200

    Update prepare.py

--- helpers/prepare.py
+++ helpers/prepare.py
@@ -30,7 +30,7 @@ elif _platform == "win32":
     # Windows
     call(["C:\Python27\Lib\site-packages\PyQt4\pyrcc4.exe", "icons.qrc", "-o" ,  "gui/qt/icons_rc.py"])
     
-    with zipfile.ZipFile('artifacts/dist/trezor-0.6.7.win32.zip', "r") as z:
+    with zipfile.ZipFile('artifacts/dist/trezor-0.6.13.win32.zip', "r") as z:
         z.extractall("artifacts")
     shutil.copy2('artifacts/Python27/Scripts/trezorctl', 'packages/trezorctl.py')
 
